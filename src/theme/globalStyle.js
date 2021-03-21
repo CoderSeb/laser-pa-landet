3faf2a0b1231f06b7eb7b18e36b6f8085b1c4755
Sebastian Åkerblom <sebbeakerblom@gmail.com>
2021-03-21 18:52:10 +0100

    Clean up and default styles added

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+html {
+  height: 100%;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+`
+
+export default GlobalStyle
---
+++ src/theme/theme.js
@@ -0,0 +1,5 @@
+export default {
+  colors: {
+    main: '#c619e0'
+  }
+}
