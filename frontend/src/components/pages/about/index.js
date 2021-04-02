d0e4aa410d440b12cafde6c8ac273bba1fa13d93
Sebastian <sa224ny@student.lnu.se>
2021-04-02 10:48:33 +0200

    Added prop right and left to TextField

--- frontend/src/components/pages/about/index.js
+++ frontend/src/components/pages/about/index.js
@@ -4,13 +4,13 @@ import TextField from '../../styled/TextField'
 const About = () => {
   return (
     <div>
-      <TextField sidePos={'right'}>
+      <TextField right>
       Mitt namn är Michaela Blomquist och jag är laserterapeut, utbildad vid Sjöbo laserklinik.<br />
-    Till vardags arbetar jag som personalchef inom kollektivtrafiken och har min mottagning
-    som deltidssysselsättning.<br />
-    Jag har min mottagning på min gård i Sjöröd, vid Gyllebosjöns vackra bokskog på
-    Österlen.<br />
-Väl mött!
+      Till vardags arbetar jag som personalchef inom kollektivtrafiken och har min mottagning
+      som deltidssysselsättning.<br />
+      Jag har min mottagning på min gård i Sjöröd, vid Gyllebosjöns vackra bokskog på
+      Österlen.<br />
+      Väl mött!
       </TextField>
     </div>
   )
--- frontend/src/components/styled/TextField.js
+++ frontend/src/components/styled/TextField.js
@@ -1,26 +1,34 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const StyledText = styled.div`
-  background: ${props => props.theme.colors.main};
   text-align:center;
   padding:1em;
-  margin:1em;
   border-radius: 10px;
   box-shadow: 0 0 5px 1px ${props => props.theme.colors.accent};
-  max-width:60%;
-  float: right;
+  max-width:80%;
+  margin: 1em auto;
+  background: ${props => props.theme.colors.main};
+
+  ${({left}) => left && css`
+    float: left;
+    margin: 1em;
+  `}
+
+  ${({right}) => right && css`
+    float: right;
+    margin: 1em;
+  `}
 
   @media only screen and (max-width: ${props => props.theme.sizes.tablet}) {
     max-width:90%;
     margin:1em auto;
-    float: initial;
   }
 `
 
-const TextField = ({children, sidePos}) => {
+const TextField = ({ children, left, right }) => {
   return (
-    <StyledText>
+    <StyledText left={left} right={right}>
     <p>
     {children}
     </p>
