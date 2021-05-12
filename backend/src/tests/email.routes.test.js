5964cef898e440a3373a042de8d87477babf1b55
Sebastian <sa224ny@student.lnu.se>
2021-05-12 11:01:43 +0200

    Another try, added modulePath

--- backend/src/tests/email.routes.test.js
+++ backend/src/tests/email.routes.test.js
@@ -1,19 +1,19 @@
 /*eslint-disable */
-import { app } from './testServer.js'
-import request from 'supertest'
+import app from './testServer.js'
+import supertest from 'supertest'
 import { emailData } from './testData.js'
 import dotenv from 'dotenv'
 import {jest} from '@jest/globals'
 dotenv.config()
 jest.useFakeTimers()
-
+const request = supertest(app)
 /**
  * Email route tests.
  */
 describe('Email tests', () => {
   // Send email with correct information.
   it('Sending email with correct information, should return 200 OK.', async done => {
-    const res = await request(app).post('/api/v1/email').send(emailData.testPayload)
+    const res = await request.post('/api/v1/email').send(emailData.testPayload)
     expect(res.statusCode).toEqual(200)
     expect(res.body).toEqual({ message: 'Ditt meddelande är skickat!' })
     done()
@@ -21,7 +21,7 @@ describe('Email tests', () => {
 
   // Send email with invalid email.
   it('Sending email with invalid email, should return 400 Bad Request.', async done => {
-    const res = await request(app).post('/api/v1/email').send(emailData.testPayloadIncorrectEmail)
+    const res = await request.post('/api/v1/email').send(emailData.testPayloadIncorrectEmail)
     expect(res.statusCode).toEqual(400)
     done()
   })
