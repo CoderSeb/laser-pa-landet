da1c28a5c7cdea2b1ba79d188be8f9339ce939fa
Sebastian <sa224ny@student.lnu.se>
2021-05-15 13:02:04 +0200

    Trying to reset modules before each

--- backend/src/tests/email.routes.test.js
+++ backend/src/tests/email.routes.test.js
@@ -10,13 +10,15 @@ const request = supertest(app)
  * Email route tests.
  */
 describe('Email tests', () => {
+  const OLD_ENV = process.env
   beforeEach(async () => {
     jest.useFakeTimers()
+    jest.resetModules()
+    process.env = { ...OLD_ENV }
   })
   // Send email with correct information.
   it('Sending email with correct information, should return 200 OK.', async done => {
     const res = await request.post('/api/v1/email').send(emailData.testPayload)
-    console.log(res)
     expect(res.statusCode).toEqual(200)
     expect(res.body).toEqual({ message: 'Ditt meddelande är skickat!' })
     done()
@@ -29,6 +31,7 @@ describe('Email tests', () => {
     done()
   })
   afterAll(async () => {
+    process.env = OLD_ENV
     app.close()
   })
 })
